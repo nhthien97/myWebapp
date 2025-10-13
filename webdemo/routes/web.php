5596192b52949e419ee24c65d93d7742b14f8114
Nguyễn Hoàng Thiên <23010139@st.phenikaa-uni.edu.vn>
2025-10-13 10:58:53 +0000

    update

--- webdemo/routes/web.php
+++ webdemo/routes/web.php
@@ -1,7 +1,11 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index']);
+Route::get('/product/{id}', [ProductController::class, 'show']);
+
+Route::get('/cart', [CartController::class, 'index']);
+Route::post('/cart/add/{id}', [CartController::class, 'add']); 

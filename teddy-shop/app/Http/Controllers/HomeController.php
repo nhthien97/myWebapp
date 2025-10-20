b3957f3c144bc2b7c487f397b99e7af79fcbdf15
Nguyễn Hoàng Thiên <23010139@st.phenikaa-uni.edu.vn>
2025-10-20 00:51:15 +0000

    web

--- teddy-shop/app/Http/Controllers/HomeController.php
+++ teddy-shop/app/Http/Controllers/HomeController.php
@@ -6,5 +6,59 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    private $products = [
+        [
+            'name' => 'Gấu Nâu Nghệ Thuật',
+            'price' => '120.000đ',
+            'image' => 'https://img2.thuthuat123.com/uploads/2020/03/23/hinh-anh-gau-bong-dep-va-nghe-thuat_103143470.jpg',
+        ],
+        [
+            'name' => 'Gấu Bing Hồng',
+            'price' => '150.000đ',
+            'image' => 'https://tse1.mm.bing.net/th/id/OIP.todAMdz-yM3mbUpkjXCMHwHaEK?pid=Api&P=0&h=220',
+        ],
+        [
+            'name' => 'Gấu Xám Trắng',
+            'price' => '130.000đ',
+            'image' => 'https://img1.kienthucvui.vn/uploads/2020/08/07/anh-gau-bong-dep_044459709.jpg',
+        ],
+        [
+            'name' => 'Gấu Cute Trắng',
+            'price' => '140.000đ',
+            'image' => 'https://img1.kienthucvui.vn/uploads/2021/01/13/anh-gau-bong-dep-cute_030009195.jpg',
+        ],
+        [
+            'name' => 'Gấu Cover Tình Yêu',
+            'price' => '160.000đ',
+            'image' => 'https://vanhoadoisong.vn/wp-content/uploads/2022/05/hinh-nen-gau-bong-cover-thumb.jpg',
+        ],
+        [
+            'name' => 'Gấu Happy To You',
+            'price' => '125.000đ',
+            'image' => 'https://thuthuatnhanh.com/wp-content/uploads/2020/03/hinh-anh-gau-bong-trang-happy-to-you.jpg',
+        ],
+        [
+            'name' => 'Logo Gấu Độc Đáo',
+            'price' => '170.000đ',
+            'image' => 'https://anhminhgift.vn/upload/blog/thumbnail/thiet-ke-logo-gau-bong-doc-dao-cho-thuong-hieu-cua-ban.jpg',
+        ]
+    ];
+
+    // Trang chủ
+    public function index()
+    {
+        $products = $this->products;
+        return view('home', compact('products'));
+    }
+
+    // Tìm kiếm sản phẩm
+    public function search(Request $request)
+    {
+        $query = strtolower($request->input('keyword'));
+        $products = collect($this->products)->filter(function ($item) use ($query) {
+            return str_contains(strtolower($item['name']), $query);
+        });
+
+        return view('home', ['products' => $products]);
+    }
 }
--- teddy-shop/routes/web.php
+++ teddy-shop/routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Trang chủ
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Tìm kiếm sản phẩm
+Route::get('/search', [HomeController::class, 'search'])->name('search');
